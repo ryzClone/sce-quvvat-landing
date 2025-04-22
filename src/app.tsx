a47a2592c1c9638179c1ada0681dbddc295fa98e
ozodbek <ozodbekjumayev50@gmail.com>
2025-04-22 15:48:07 +0500

    asd

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 // import ScrollToTopButton from './components/ScrollTopButton/ScrollToTopButton';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Contact from './components/Contacts/Contact';
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+
+      <section id="contact">
+        <Contact />
+      </section>
 
       {/* <ScrollToTopButton /> */}
     </>
